b9f5041b75b7f6c5414b84bd664e55e73d3abeaa
Harry Catraz <harrylong0905@gmail.com>
2019-09-10 18:45:07 +0100

    Fixed Underwater HUD and Chat

--- [Retaliation Gaming]/RG_Client/functions/UI/fnc_UnderwaterHUD.lua
+++ [Retaliation Gaming]/RG_Client/functions/UI/fnc_UnderwaterHUD.lua
@@ -1,16 +1,23 @@
-function textOxygen(content) 
+function textOxygen(string, content) 
     SetTextFont(1)
     SetTextProportional(0)
     SetTextScale(0.5,0.5)
     SetTextEntry("STRING")
-    AddTextComponentString("Oxygen Left: " ..content.."Secs")
-    DrawText(0.88,0.89)
+    AddTextComponentString(string .. content.."Secs")
+    DrawText(0.88,0.86)
 end
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(2)
+        Citizen.Wait(0)
+        SetPedMaxTimeUnderwater(GetPlayerPed(-1), 15.0)  
         if IsPedSwimmingUnderWater(GetPlayerPed(-1)) and not IsPedInVehicle(GetPlayerPed(-1)) then 
-            textOxygen(math.floor(GetPlayerUnderwaterTimeRemaining(GetPlayerPed(-1))))
+            breath = GetPlayerUnderwaterTimeRemaining(PlayerId())
+            breathrounded = math.floor(breath * 10) / 10 
+            if breathrounded <= 0 then
+                textOxygen("Drowning For: " ,breathrounded)
+            else
+                textOxygen("Oxygen Left: ", breathrounded)
+            end
         end
     end
 end)
